fa60f64ad69f0cc30a8c2bd7df6eab027cf1a0ea
simon <simondiril@gmail.com>
2022-08-31 16:07:28 +0200

    teste apparition

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,19 +1,17 @@
-const scroll = new LocomotiveScroll ({
-    el: document.querySelector('[data-scroll-container]'),
-    smooth : true,
-    tablet : { smooth : true },
-    smartphone : { smooth : true},
-});
-
-
-// body color
-scroll.on("scroll", () => {
-    if (document.querySelector("#color.is-invew")) {
-        document.body.style.background = "#000101";
-        document.body.style.color = "#fefeff";
+const scroll = new LocomotiveScroll({
+    el: document.querySelector("[data-scroll-container]"),
+    smooth: true,
+    tablet: { smooth: true },
+    smartphone: { smooth: true },
+  });
+  
+  // Body color
+  scroll.on("scroll", () => {
+    if (document.querySelector("#color.is-inview")) {
+      document.body.style.background = "#000101";
+      document.body.style.color = "#fefeff";
     } else {
-        document.body.style.background = "#fefeff";
-        document.body.style.color = "#000101";
+      document.body.style.background = "#fefeff";
+      document.body.style.color = "#000101";
     }
-});
-
+  });
